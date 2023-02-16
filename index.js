3dd19c59e4e1618a7bce4beb2f9feb3a6884e6b6
trihuang <124452877+trihuang@users.noreply.github.com>
2023-02-17 04:41:27 +0800

    Add button instead of displaying subtext and add functionality of changing grid size

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector('.grid');
 const gridSize = document.getElementById('grid-size');
+let squares = document.querySelectorAll('.square');
 
 // Initialize a 16 x 16 grid
 
@@ -9,19 +10,48 @@ function initializeGrid() {
         div.classList.add("square");
         grid.appendChild(div);
     }
+    updateSquareListeners();
 }
 
+// Add EventListener to every square in the grid
+
+function updateSquareListeners() {
+    squares = document.querySelectorAll('.square');
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('mouseover', colorSquare);
+    }
+}
+
+// Get number input from user
+
+function inputNum() {
+    let input = prompt('Please enter an integer from 1 to 100 to specify the number of squares on each side of the new grid:');
+    if (input != null) {
+        input = parseInt(input);
+        if (isNaN(input) || input < 1 || input > 100) {
+            alert('Please enter an integer from 1 to 100.');
+        } else updateGrid(input); 
+    }
+}
+
+// Update the grid size based on the user input
+
+function updateGrid(num) {
+    let newGridSize = num * num;
+    grid.innerHTML = "";
+    grid.style.gridTemplateColumns = 'repeat('+num+', 1fr)';
+    grid.style.gridTemplateRows = 'repeat('+num+', 1fr)';
+    for (let i = 0; i < newGridSize; i++) {
+        let div = document.createElement("div");
+        div.classList.add("square");
+        grid.appendChild(div);
+    }
+    updateSquareListeners();
+}
 
 function colorSquare(e) {
     e.target.style.backgroundColor = 'black';
 }
 
-
 initializeGrid();
 
-// Add EventListener to every square in the grid
-
-const squares = document.querySelectorAll('.square');
-for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('mouseover', colorSquare);
-}
